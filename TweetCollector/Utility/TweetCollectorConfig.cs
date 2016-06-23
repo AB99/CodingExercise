08fce2cc7ad57b15ee1dd334dfd4d10e66ab3503
Andy <Andy@FRYLOCK>
2016-06-22 18:05:48 -0700

    Visual Studio somehow lost my previous changes. Neat.

--- TweetCollector/Utility/TweetCollectorConfig.cs
+++ TweetCollector/Utility/TweetCollectorConfig.cs
@@ -1,12 +1,39 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-namespace TweetCollector
+using System.Configuration;
+using System.IO;
+
+namespace TweetCollector.Utility
 {
     public class TweetCollectorConfig
     {
-        public string 
+        readonly Configuration _config;
+
+        public TweetCollectorConfig(string configFile = "TweetCollectorConfig.xml")
+        {
+            var fileMap = new ExeConfigurationFileMap
+            {
+                ExeConfigFilename = Path.Combine(Environment.CurrentDirectory, configFile)
+            };
+
+            _config = ConfigurationManager.OpenMappedExeConfiguration(fileMap, ConfigurationUserLevel.None);
+
+        }
+
+        private string GetConfigValue(string key)
+        {
+            var configElement = _config.AppSettings.Settings[key];
+            return configElement?.Value;
+        }
+
+        public string TwitterApiUrl => GetConfigValue("TwitterApiUrl");
+
+        //Tokens identifying the app
+        public string ConsumerKey => GetConfigValue("ConsumerKey");
+        public string ConsumerSecret => GetConfigValue("ConsumerSecret");
+
+        //Tokens identifying the individual twitter account
+        public string AccessToken => GetConfigValue("AccessToken");
+        public string AccessTokenSecret => GetConfigValue("AccessTokenSecret");
+
     }
 }
