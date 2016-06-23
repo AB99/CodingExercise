87e204066c8b6646a9ffeb07cb76661c40deb8d2
Andy <Andy@FRYLOCK>
2016-06-22 17:46:23 -0700

    Adding config

---
+++ TweetCollector/Model/TwitterStatus.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.Serialization;
+using Newtonsoft.Json;
+
+namespace TweetCollector.Model
+{
+    [JsonObject(MemberSerialization.OptIn)]
+    [DebuggerDisplay("{User.ScreenName}/{Text}")]
+    [DataContract]
+    public class BlarghTwitterStatus
+    {
+        [DataMember, JsonProperty(PropertyName = "id")]
+        public decimal Id { get; set; }
+
+        [DataMember]
+        [JsonProperty(PropertyName = "created_at")]
+        [JsonConverter(typeof(TwitterDateTimeConverter))]
+        public DateTime CreatedDate { get; set; }
+
+        [DataMember, JsonProperty(PropertyName = "source")]
+        public string Source { get; set; }
+
+        [DataMember, JsonProperty(PropertyName = "text")]
+        public string Text { get; set; }
+
+        [DataMember, JsonProperty(PropertyName = "user")]
+        public TwitterUser User { get; set; }
+    }
+}
