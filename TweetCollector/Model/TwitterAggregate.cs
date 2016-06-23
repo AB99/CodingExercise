f1a2a4bc782d7a6e913021236a2b49ace94556a6
Andy <Andy@FRYLOCK>
2016-06-22 21:11:06 -0700

    adding tweet collector implementation

---
+++ TweetCollector/Model/TwitterAggregate.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
+using Newtonsoft.Json;
+
+namespace TweetCollector.Model
+{
+    [JsonObject(MemberSerialization.OptIn)]
+    [DataContract]
+    public class TwitterAggregate
+    {
+        [DataMember, JsonProperty(PropertyName = "tweet_count")]
+        public int TweetCount { get; set; }
+
+        [DataMember, JsonProperty(PropertyName = "tweets_mentioning_users_count")]
+        public int TweetsMentioningUsersCount { get; set; }
+
+        [DataMember, JsonProperty(PropertyName = "tweets")]
+        public IList<TwitterStatus> Tweets { get; set; }
+    }
+}
--- TweetCollector/TweetCollector.cs
+++ TweetCollector/TweetCollector.cs
@@ -3,10 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TweetCollector.Model;
 
 namespace TweetCollector
 {
-    class TweetCollector
+    public class TweetCollector
     {
+        private readonly ITwitterFacade _twitterFacade;
+        private readonly string[] _screenNamesOfInterest = { "@pay_by_phone", "@PayByPhone", " @PayByPhone_UK" };
+
+        public TweetCollector(ITwitterFacade twitterFacade)
+        {
+            _twitterFacade = twitterFacade;
+        }
+
+        public TwitterAggregate CollectTweetsForLastTwoWeeks()
+        {
+            var tweets = new List<TwitterStatus>();
+
+            foreach (string screenName in _screenNamesOfInterest)
+            {
+                var userTimelineTweets = _twitterFacade.UserTimeLine(screenName);
+                tweets.AddRange(userTimelineTweets);
+            }
+
+            tweets = tweets
+                .Where(t => t.CreatedDateGmt >= DateTimeOffset.UtcNow.AddDays(-14))
+                .OrderByDescending(t => t.CreatedDateGmt).ToList();
+
+            int tweetsMentioningUsersCount = tweets.Count(t => t.Text.Contains("@"));
+
+            var aggregate = new TwitterAggregate
+            {
+                TweetCount =  tweets.Count,
+                TweetsMentioningUsersCount = tweetsMentioningUsersCount,
+                Tweets = tweets
+            };
+
+            return aggregate;
+        }
     }
 }
